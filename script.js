c69576d3ba193d90a6bfc8fd3cda8bbf466a5aff
J <118442144+model-map@users.noreply.github.com>
2023-02-13 16:46:24 +0530

    no operator at start or two operators together

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 const buttons=document.querySelectorAll(`[data-key]`);
 const screen=document.querySelector('.screen');
+let expression=null;
+
+const keys=['(', ')', '%', '/', '*', '-', '+', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const keysNum=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const keysOperators=['(', ')', '%', '/', '*', '-', '+', '.'];
 
 // Event Listeners
 
     // CLICK
     buttons.forEach((button)=>{
         button.addEventListener("mousedown",(e)=>{
+            expression=screen.textContent.trim(); // remove whitespace
             const dataKey= button.getAttribute('data-key');
-            if (dataKey=='CE'){
+
+            if(dataKey=='=') calculate(expression);
+            else if (dataKey=='CE'){
                 screen.textContent=screen.textContent.slice(0,-1);
             }
-            else if (checkLength(screen.textContent)){
-                const text=button.textContent;
-                screen.textContent+=text;}
+            else if (checkLength(expression)){
+                if (expression.length==0 && keysNum.includes(dataKey))
+                {screen.textContent+=dataKey;
+            }
+                else if(expression.length>0){
+                    if (keysOperators.includes(dataKey)){
+                        if (!keysOperators.includes(expression.slice(-1))){
+                            screen.textContent+=dataKey;}
+                    }
+                    else screen.textContent+=dataKey;
+                }
+            }
         })
     })
 
@@ -20,7 +37,6 @@ const screen=document.querySelector('.screen');
     window.addEventListener("keydown",(e)=>{
         //console.log(e['key']);
         const key=e['key'];
-        const keys=['(', ')', '%', '/', '*', '-', '+', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
         if (keys.includes(key)){
             e.preventDefault();         // prevents quick find associated with / button
@@ -32,12 +48,39 @@ const screen=document.querySelector('.screen');
         }
         else if (key=='=' || key=='Enter'){
             // Evaluate 
-            console.log("I need to add fn to evaluate expression here :| ")
+            calculate(expression);
         }
+        expression=screen.textContent;
+
+
     })
 
 // Functions
 
+
 function checkLength (expression){
-    return expression.length-10<=40?true:false;
+    expression=expression.trim();
+    return expression.length<=40?true:false;
+}
+
+function calculate(expression){
+    expression=expression.trim();
+    console.log("Yeah i'll work soon dont worry :/ ");
+    console.log("Here, meanwhile check if the expression is ok");
+    console.log("-----------EXPRESSION----------");
+    console.log(expression);
+
+    // check expression
+        // 1. should have numbers on first and last index
+        // 2. should have at least two numbers and one operator
+        //3. should have n numbers and n-1 operators
+
+    console.log(checkExpression(expression));
+    
+    
+
+}
+
+function checkExpression(expression){
+    return keysNum.includes(expression.slice(-1))?true:false;
 }
