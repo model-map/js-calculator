82313a55595166e59c2b5053a700d8d6203fb332
J <118442144+model-map@users.noreply.github.com>
2023-02-13 13:41:17 +0530

    Delete last input with CE btn and Backspace key

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ const screen=document.querySelector('.screen');
     buttons.forEach((button)=>{
         button.addEventListener("click",(e)=>{
             const dataKey= button.getAttribute('data-key');
-            if (dataKey=='CE') screen.textContent='';
+            if (dataKey=='CE'){
+                screen.textContent=screen.textContent.slice(0,-1);
+            }
             else {
                 const text=button.textContent;
                 screen.textContent+=text;}
@@ -15,20 +17,26 @@ const screen=document.querySelector('.screen');
     })
 
     //Keyboard
-    window.addEventListener("keypress",(e)=>{
-        e.preventDefault();         // prevents quick find associated with / button
+    window.addEventListener("keydown",(e)=>{
         //console.log(e['key']);
         const key=e['key'];
         const keys=['(', ')', '%', '/', '*', '-', '+', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
         if (keys.includes(key)){
+            e.preventDefault();         // prevents quick find associated with / button
             const text=key;
             screen.textContent+=key;
+        }
+        else if (key=='Backspace'){
+            screen.textContent=screen.textContent.slice(0,-1);
         }
         else if (key=='=' || key=='Enter'){
             // Evaluate 
             console.log("I need to add fn to evaluate expression here :| ")
         }
     })
+
+// Backspace and CE function
 
     /*
     Key     |NumBar     |NumPad
